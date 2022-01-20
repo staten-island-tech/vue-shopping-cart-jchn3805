1a32ea738a02103fb155ff68b951f994336afdf2
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-01-19 19:42:37 -0500

    x

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <h1 v-if="login">{{status[1]}}</h1>
+    <h1 v-else>{{status[0]}}</h1>
+    <ul>
+      <li v-for="animal in animals" :key="animal">{{animal}}</li>
+    </ul>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -13,6 +17,13 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      status: ["login", "logout"],
+      login: false,
+      animals: ["dog", "cat", "pig", "cow", "hen"]
+    }
   }
 }
 </script>
