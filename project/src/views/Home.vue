4051a1a73b26cf50e1d0114c10f4f94ec247b091
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-01-19 20:16:27 -0500

    x

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="home">
-    <h1 v-if="login">{{status[1]}}</h1>
-    <h1 v-else>{{status[0]}}</h1>
+    <button v-on:click="authState" v-if="loggedIn">{{status[0]}}</button>
+    <button @click="authState" v-else>{{status[1]}}</button>
     <ul>
       <li v-for="animal in animals" :key="animal">{{animal}}</li>
     </ul>
+    <input type="text" placeholder="type here" v-model="message">
+    <p>{{message}}</p>
+    <input type="checkbox" id="jack" value="jack" v-model="checkedNames">
+    <label for="jack">jack</label>
+    <input type="checkbox" id="john" value="john" v-model="checkedNames">
+    <label for="john">john</label>
+    <input type="checkbox" id="mike" value="mike" v-model="checkedNames">
+    <label for="mike">mike</label>
+    <br>
+    <span>Checked names: {{checkedNames}}</span>
+    <br>
+    <select v-model="selected">
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+    </select>
+    <br>
+    <span>{{selected}}</span>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -20,10 +38,44 @@ export default {
   },
   data() {
     return {
-      status: ["login", "logout"],
-      login: false,
-      animals: ["dog", "cat", "pig", "cow", "hen"]
+      status: ["logout", "login"],
+      loggedIn: false,
+      animals: ["dog", "cat", "pig", "cow", "hen"],
+      message: "",
+      checkedNames: [],
+      selected: "",
+    }
+  },
+  methods: {
+    authState: function () {
+      if(this.loggedIn===false) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false
+      }
     }
   }
 }
 </script>
+
+<style>
+  button {
+    background-color: #151515;
+    color: #fff;
+    border: none;
+    border-radius: 0.2rem;
+    height: 2rem;
+    width: 5rem;
+    font-size: 1rem;
+    transition: all 0.3s;
+  }
+
+  button:hover {
+    background-color: rgba(21, 21, 21, 0.7);
+    cursor: pointer;
+  }
+
+  li {
+    list-style: none;
+  }
+</style>
