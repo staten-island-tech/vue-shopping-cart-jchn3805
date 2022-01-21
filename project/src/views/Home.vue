b3fd94fdf6e9a376ac9016babf30709f25680eeb
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-01-21 11:16:33 -0500

    x

--- project/src/views/Home.vue
+++ project/src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="home">
-    <!-- <Card 
-    name="Lorem ipsum"
-    price="$$$"
-    image="https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
-    /> -->
-    <Card v-for="destination in destinations" :key="destination" :title="destinations.name"/>
+    <Card 
+    v-for="destination in destinations"
+    :key="destination" 
+    :name="destination.name" 
+    :price="`$${destination.price}`" 
+    :image="destination.image"
+    />
   </div>
 </template>
 
@@ -19,13 +20,27 @@ export default {
   components: {
     Card
   },
-  destinations: [
-    {
-      name: "Lorem ipsum",
-      price: 100,
-      image: "https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+  data(){
+    return {
+      destinations: [
+        {
+          name: "Lorem ipsum",
+          price: 100,
+          image: "https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        },
+        {
+          name: "Lorem ipsum",
+          price: 100,
+          image: "https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        },
+        {
+          name: "Lorem ipsum",
+          price: 100,
+          image: "https://images.unsplash.com/photo-1642427749670-f20e2e76ed8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
+        },
+      ]
     }
-  ]
+  }
 }
 </script>
 
