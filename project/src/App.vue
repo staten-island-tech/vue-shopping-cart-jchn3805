38f694ab93a3dce1fd761029ee725b07d1eaa687
jasonchen3805 <90630687+jchn3805@users.noreply.github.com>
2022-01-29 11:05:21 -0500

    Update App.vue

--- project/src/App.vue
+++ project/src/App.vue
@@ -254,14 +254,13 @@ export default {
     addCart(index) {
       this.inCart.push(this.items[index])
       this.cartPrice.push(this.items[index].price)
-      this.subtotal = this.cartPrice.reduce(function (a, b) {
-        return a + b
-      })
+      this.subtotal = this.cartPrice.reduce(((x, y) => x + y))
       this.cartItems = this.cartItems + 1
     },
     removeItem(index) {
       this.inCart.splice(index, 1)
-      this.subtotal = this.cartPrice - this.cartPrice[index]
+      this.cartPrice.splice(index, 1)
+      this.subtotal = this.cartPrice.reduce(((x, y) => x + y))
       this.cartItems = this.cartItems - 1 
     }
   }
